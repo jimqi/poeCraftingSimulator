69183b8a0a54f3b407123b892a211dcf908a7b99
JimQi <jim.qi101@gmail.com>
2015-03-16 22:37:55 -0700

    Added JUnit tests
    changed committer email to actual email

---
+++ poeCraftingSim/test/poeCraftingSim/client/orbs/ExaltedOrbTest.java
@@ -0,0 +1,14 @@
+package poeCraftingSim.client.orbs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExaltedOrbTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
